5a0d5a3503a2aec9da57eb3187bfa18005fa008a
Julian <birdman.coding.1991@gmail.com>
2024-06-11 11:57:55 +0200

    implement shuffle start

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ const appDiv = document.getElementById('app') as HTMLDivElement;
 
 const numCards = 24; // total number of cards
 
+// create new array with IDs for cards
+let memoryCards: number[] = [];
+for (let i=1; i<=numCards; i++) { memoryCards.push(i) };
+
+// shuffle the array
+memoryCards.sort(() => Math.random()-0.5 );
+
 // fill board with cards
-for (let i=0; i<numCards; i++) {
+for (let i of memoryCards) {
   // create new div
   let newCard = document.createElement('div') as HTMLDivElement;
   // add class to div
   newCard.classList.add('memory__card');
+  newCard.innerHTML = `<img src="/img/${i%12+1}.svg" />`;
   // append class to appDiv
   appDiv.appendChild(newCard);
 }
