ae23539a3c8c1dbad98a2dea4c147b9ee3dc7b33
Julian <birdman.coding.1991@gmail.com>
2024-06-11 10:23:25 +0200

    add cards

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 // --------------- MEMORY GAME --------------- //
 
-// create Memory Board
-// use: document.getElementById()
-// use: document.createElement()
-// use: a FOR loop
+const appDiv = document.getElementById('app') as HTMLDivElement;
+
+const numCards = 24; // total number of cards
+
+// fill board with cards
+for (let i=0; i<numCards; i++) {
+  // create new div
+  let newCard = document.createElement('div') as HTMLDivElement;
+  // add class to div
+  newCard.classList.add('memory__card');
+  // append class to appDiv
+  appDiv.appendChild(newCard);
+}
