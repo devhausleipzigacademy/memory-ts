53550ea10cc5012e0acafc4a2da3572d2926d24b
Julian <birdman.coding.1991@gmail.com>
2024-06-11 12:27:43 +0200

    implement show on click

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 // --------------- MEMORY GAME --------------- //
 
+// --------------- SET UP GAME --------------- //
+
 const appDiv = document.getElementById('app') as HTMLDivElement;
 
 const numCards = 24; // total number of cards
@@ -17,7 +19,16 @@ for (let i of memoryCards) {
   let newCard = document.createElement('div') as HTMLDivElement;
   // add class to div
   newCard.classList.add('memory__card');
-  newCard.innerHTML = `<img src="/img/${i%12+1}.svg" />`;
+  newCard.innerHTML = `<img src="/img/${i%12+1}.svg" id=${i} class="hidden" />`;
+  newCard.addEventListener('click', () => handleClick(i));
   // append class to appDiv
   appDiv.appendChild(newCard);
+}
+
+// --------------- RUN GAME --------------- //
+
+// TODO: Make card visible on click
+function handleClick(id: number) {
+  const card = document.getElementById(id.toString()) as HTMLElement;
+  card.classList.toggle('hidden');
 }
